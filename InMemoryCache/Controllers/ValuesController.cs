8e5b1c716e4a336a917c2248cf999968f6ee6328
uzunsahin <sahin.uzun@gratas.com.tr>
2023-04-26 21:36:58 +0300

    Add project files.

---
+++ InMemoryCache/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCache.Controllers
+{
+    [Route("api/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+        public ValuesController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public void SetName()
+        {
+            _memoryCache.Set("name", "sahin"); 
+        }
+
+        [HttpGet]
+        public string GetName()
+        {
+            return _memoryCache.Get<string>("name"); 
+        }
+
+        [HttpGet]
+        public string GetName2()
+        {
+            if (_memoryCache.TryGetValue<string>("name",out string name))
+            {
+                return name.Substring(3);
+            }
+            return "";
+
+        }
+
+        [HttpGet]
+        public void SetDate()
+        {
+            _memoryCache.Set<DateTime>("date", DateTime.UtcNow, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),//30 sn boyunca burada atılan veriye ulaaşabilirim. Sliding Expiration maksimum 30 sn boyunca olur gibi düşünebilirsiniz.
+                SlidingExpiration = TimeSpan.FromSeconds(5), //Eğer bu veriyi 5sn boyunca kullanmazsan bu veri direk silinecektir.
+            });  
+        }
+
+        [HttpGet]
+        public DateTime GettDate()
+        {
+            return _memoryCache.Get<DateTime>("date");  
+        }
+    }
+}
